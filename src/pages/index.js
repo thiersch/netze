1d600783da3c2d3b53a3882d94545dd20be492ac
thiersch <34606632+thiersch@users.noreply.github.com>
2025-09-18 22:07:22 +0200

    commented out link

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,7 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
+        /*
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
@@ -23,6 +24,7 @@ function HomepageHeader() {
             Los gehts!
           </Link>
         </div>
+        */
       </div>
     </header>
   );
