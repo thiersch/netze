a749bd278456348c16b575eeab0d7e182d437f08
thiersch <34606632+thiersch@users.noreply.github.com>
2025-09-18 22:02:36 +0200

    index to index.md

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/index">
+            to="/docs/index.md">
             Los gehts!
           </Link>
         </div>
